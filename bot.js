e6ac3d040d278cf2773cd35c06d93396f3556846
suhaaa <44467890+suhaaa@users.noreply.github.com>
2019-02-23 11:16:12 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`$rep @FT-Like.#0001 [ " ${x} " ]`)
+        message.channel.send(`$rep @FT-Like.#0001 `)
           .then(m => {
             count++;
           })
